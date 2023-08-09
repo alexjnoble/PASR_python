15dc45c8b50b2adebd06e04b0e255ead62b8dabd
alexjnoble <anoble@nysbc.org>
2023-08-09 18:37:43 -0400

    Added error handling

--- pasr.py
+++ pasr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Author: Alex J. Noble with help from GPT4, June-July 2023
+# Author: Alex J. Noble with help from GPT4, June-August 2023
 # Associated manuscript: https://doi.org/10.1101/2023.06.09.544325
 #
 # This script applies the PASR algorithm to 2D images or image stacks.
@@ -34,7 +34,7 @@ def scale_image(image, scale):
     return np.repeat(np.repeat(image, scale, axis=1), scale, axis=0)
 
 # Function to process each file
-def process_file(input_file, output_file, scale, compression, flip_tif):
+def process_file(input_file, output_file, scale, compression, flip_tif, jpg_quality):
     """Processes a file: reads, scales and writes it.
 
     Args:
@@ -47,67 +47,69 @@ def process_file(input_file, output_file, scale, compression, flip_tif):
     Returns:
     None
     """
-    # Check if output is JPG and read header to check dimensions
-    if output_file.endswith('.jpg'):
+    try:
+        # Check if output is JPG and read header to check dimensions
+        if output_file.endswith('.jpg'):
+            if input_file.endswith('.mrc') or input_file.endswith('.mrcs'):
+                with mrcfile.mmap(input_file, mode='r') as mrc:
+                    if mrc.header['nz'] > 1:
+                        print(colored(f'Error: Cannot save 2D frame stack as JPG for {input_file}', 'red'))
+                        return
+            elif input_file.endswith('.tif') or input_file.endswith('.tiff'):
+                with TiffFile(input_file) as tif:
+                    original_data_dim = len(tif.series[0].shape)
+                    if original_data_dim > 2:
+                        print(colored(f'Error: Cannot save 2D frame stack as JPG for {input_file}', 'red'))
+                        return
+
+        # Reading the file
         if input_file.endswith('.mrc') or input_file.endswith('.mrcs'):
-            with mrcfile.mmap(input_file, mode='r') as mrc:
-                if mrc.header['nz'] > 1:
-                    print(colored('Error: Cannot save 2D frame stack as JPG.', 'red'))
-                    return
+            with mrcfile.open(input_file) as mrc:
+                original_data = mrc.data
         elif input_file.endswith('.tif') or input_file.endswith('.tiff'):
-            with TiffFile(input_file) as tif:
-                original_data_dim = len(tif.series[0].shape)
-                if original_data_dim > 2:
-                    print(colored('Error: Cannot save 2D frame stack as JPG.', 'red'))
-                    return
-
-    # Reading the file
-    if input_file.endswith('.mrc') or input_file.endswith('.mrcs'):
-        with mrcfile.open(input_file) as mrc:
-            original_data = mrc.data
-    elif input_file.endswith('.tif') or input_file.endswith('.tiff'):
-        original_data = imread(input_file)
-    elif input_file.endswith('.jpg') or input_file.endswith('.jpeg'):
-        original_data = imageio.imread(input_file)
-    else:
-        raise ValueError(colored('Unsupported input file format. Only MRC, TIF and JPG files are supported.', 'red'))
-
-    # Scaling the image
-    if original_data.ndim == 3:
-        processed_data = np.empty((original_data.shape[0], original_data.shape[1]*scale, original_data.shape[2]*scale), dtype=original_data.dtype)
-        for i in range(original_data.shape[0]):
-            processed_data[i] = scale_image(original_data[i], scale)
-    elif original_data.ndim == 2:
-        processed_data = scale_image(original_data, scale)
-    else:
-        raise ValueError(colored('Unsupported data dimensions. Only 2D images and 2D frame stacks are supported.', 'red'))
-
-    # Writing the output
-    if output_file.endswith('.mrc') or output_file.endswith('.mrcs'):
-        with mrcfile.new(output_file, overwrite=True) as mrc:
-            mrc.set_data(processed_data)
-        print(colored(f'PASR pre-processed data written to {output_file}. No compression applied for MRC format (use TIF for compression).', 'green'))
-    elif output_file.endswith('.tif') or output_file.endswith('.tiff'):
-        if flip_tif:
-            processed_data = np.flip(processed_data, axis=0)
-            flip_status = "Flipping applied."
+            original_data = imread(input_file)
+        elif input_file.endswith('.jpg') or input_file.endswith('.jpeg'):
+            original_data = imageio.imread(input_file)
         else:
-            flip_status = "No flipping applied."
-        imwrite(output_file, processed_data, compression=compression)
-        print(colored(f'PASR pre-processed data written to {output_file} with {compression} compression. {flip_status}', 'green'))
-    elif output_file.endswith('.jpg'):
-        processed_data = processed_data.astype(np.uint8)
-        im = Image.fromarray(processed_data)
-        # Append the JPEG quality to the filename
-        base_name, ext = os.path.splitext(output_file)
-        output_file = f"{base_name}_JPG{args.jpg_quality}{ext}"
-        im.save(output_file, quality=args.jpg_quality)
-        print(colored(f'PASR pre-processed data written to {output_file} with JPEG quality={args.jpg_quality}', 'green'))
-    else:
-        raise ValueError(colored('Unsupported output file format. Only MRC, TIF, and JPG for 2D images are supported.', 'red'))
+            raise ValueError(colored(f'Unsupported input file format for {input_file}. Only MRC, TIF and JPG files are supported.', 'red'))
+
+        # Scaling the image
+        if original_data.ndim == 3:
+            processed_data = np.empty((original_data.shape[0], original_data.shape[1]*scale, original_data.shape[2]*scale), dtype=original_data.dtype)
+            for i in range(original_data.shape[0]):
+                processed_data[i] = scale_image(original_data[i], scale)
+        elif original_data.ndim == 2:
+            processed_data = scale_image(original_data, scale)
+        else:
+            raise ValueError(colored(f'Unsupported data dimensions for {input_file}. Only 2D images and 2D frame stacks are supported.', 'red'))
+
+        # Writing the output
+        if output_file.endswith('.mrc') or output_file.endswith('.mrcs'):
+            with mrcfile.new(output_file, overwrite=True) as mrc:
+                mrc.set_data(processed_data)
+            print(colored(f'PASR pre-processed data written to {output_file}. No compression applied for MRC format (use TIF for compression).', 'green'))
+        elif output_file.endswith('.tif') or output_file.endswith('.tiff'):
+            if flip_tif:
+                processed_data = np.flip(processed_data, axis=0)
+                flip_status = "Flipping applied."
+            else:
+                flip_status = "No flipping applied."
+            imwrite(output_file, processed_data, compression=compression)
+            print(colored(f'PASR pre-processed data written to {output_file} with {compression} compression. {flip_status}', 'green'))
+        elif output_file.endswith('.jpg'):
+            processed_data = processed_data.astype(np.uint8)
+            im = Image.fromarray(processed_data)
+            base_name, ext = os.path.splitext(output_file)
+            output_file = f"{base_name}_JPG{jpg_quality}{ext}"
+            im.save(output_file, quality=jpg_quality)
+            print(colored(f'PASR pre-processed data written to {output_file} with JPEG quality={jpg_quality}', 'green'))
+        else:
+            raise ValueError(colored(f'Unsupported output file format for {input_file}. Only MRC, TIF, and JPG for 2D images are supported.', 'red'))
+    except Exception as e:
+        print(colored(f'An error occurred while processing file {input_file}: {e}', 'red'))
 
 # Function to process a directory
-def process_files(file_list, output_dir, scale, compression, flip_tif, force_tif, force_mrc, force_jpg, n_cores, keep_basename):
+def process_files(file_list, output_dir, scale, compression, flip_tif, force_tif, force_mrc, force_jpg, n_cores, keep_basename, jpg_quality):
     """Processes a directory: lists all files and processes each of them.
 
     Args:
@@ -125,6 +127,10 @@ def process_files(file_list, output_dir, scale, compression, flip_tif, force_tif
     Returns:
     None
     """
+    # Filter files by supported extensions
+    supported_extensions = ['.mrc', '.mrcs', '.tif', '.tiff', '.jpg', '.jpeg']
+    file_list = [file for file in file_list if os.path.splitext(file)[1].lower() in supported_extensions]
+
     # Get the number of files and the minimum of cores and number of files
     num_files = len(file_list)
     min_cores = min(n_cores, num_files)
@@ -154,7 +160,7 @@ def process_files(file_list, output_dir, scale, compression, flip_tif, force_tif
             output_file = os.path.join(output_dir, f"{base_name}_PASR_{scale}x{ext}")
         if flip_tif is None:
             flip_tif = input_file.lower().endswith(('.mrc', '.mrcs')) and output_file.lower().endswith(('.tif', '.tiff'))
-        arguments.append((input_file, output_file, scale, compression, flip_tif))
+        arguments.append((input_file, output_file, scale, compression, flip_tif, jpg_quality))
 
     # Using a multiprocessing pool to process all files in parallel
     with Pool(n_cores) as p:
@@ -167,7 +173,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--scale", type=int, choices=range(2, 6), default=2, help="Scaling factor (number of times to duplicate each pixel)")
     parser.add_argument("-o", "--output", type=str, help="Output MRC, TIF, or TIFF file, or directory to store processed files. If not provided and the input is a directory, the input directory is used.")
     parser.add_argument("-c", "--compression", type=str, choices=['zlib', 'lzw'], default='lzw', help="Compression algorithm for TIF output. ZLIB is smaller, but may not work with some software.")
-    parser.add_argument("-q", "--jpg_quality", type=int, default=95, help="Quality for JPG output, from 1 (worst) to 100 (best). Note: values above 95 may not increase quality much, but will increase file size substantially. Default is 95.")
+    parser.add_argument("-q", "--jpg_quality", type=int, default=95, choices=range(1, 101), help="Quality for JPG output, from 1 (worst) to 100 (best). Note: values above 95 may not increase quality much, but will increase file size substantially. Default is 95.")
     parser.add_argument("-n", "--n_cores", type=int, default=os.cpu_count(), help="Number of CPU cores to use for parallel processing.")
     parser.add_argument("-k", "--keep_basename", action="store_true", help="Keep the original basename for the output file(s); do not append _PASR_{scale}x.")
     parser.add_argument("-f", "--flip_tif", default=None, action='store_true', help="Option to flip TIF output across the x-axis. Default is True for MRC/MRCS input and TIF/TIFF output, False otherwise, unless specified.")
@@ -188,10 +194,10 @@ if __name__ == "__main__":
 
         # Add user confirmation for potential file overwriting
         if os.path.normpath(os.path.dirname(os.path.abspath(args.input[0]))) == os.path.normpath(args.output) and not args.yes:
-            confirm = input(colored('Warning: Your input and output directories are the same. This may overwrite your files. Continue? [y/N]', 'yellow'))
+            confirm = input(colored(f'Warning: Your input and output directories are the same ({args.output}). This may overwrite your files. Continue? [y/N]', 'yellow'))
             if confirm.lower() != 'y':
                 exit()
-        process_files(args.input, args.output, args.scale, args.compression, args.flip_tif, args.tif, args.mrc, args.jpg, args.n_cores, args.keep_basename)
+        process_files(args.input, args.output, args.scale, args.compression, args.flip_tif, args.tif, args.mrc, args.jpg, args.n_cores, args.keep_basename, args.jpg_quality)
 
     # Check if the input is a directory
     elif os.path.isdir(args.input[0]):
@@ -205,12 +211,12 @@ if __name__ == "__main__":
 
         # Add user confirmation for potential file overwriting
         if os.path.normpath(input_path) == os.path.normpath(args.output) and not args.yes:
-            confirm = input(colored('Warning: Your input and output directories are the same. This may overwrite your files. Continue? [y/N]', 'yellow'))
+            confirm = input(colored(f'Warning: Your input and output directories are the same ({args.output}). This may overwrite your files. Continue? [y/N]', 'yellow'))
             if confirm.lower() != 'y':
                 exit()
 
         file_list = glob.glob(os.path.join(input_path, "*"))
-        process_files(file_list, args.output, args.scale, args.compression, args.flip_tif, args.tif, args.mrc, args.jpg, args.n_cores, args.keep_basename)
+        process_files(file_list, args.output, args.scale, args.compression, args.flip_tif, args.tif, args.mrc, args.jpg, args.n_cores, args.keep_basename, args.jpg_quality)
     
     # Check if the input is one file
     elif len(args.input) == 1:
@@ -240,11 +246,11 @@ if __name__ == "__main__":
 
         # Add user confirmation for potential file overwriting
         if os.path.exists(args.output) and not args.yes:
-            confirm = input(colored('Warning: Your output file already exists. This will overwrite your file. Continue? [y/N]', 'yellow'))
+            confirm = input(colored(f'Warning: Your output file already exists ({args.output}). This will overwrite your file. Continue? [y/N]', 'yellow'))
             if confirm.lower() != 'y':
                 print(colored(f'Skipping file {input_path}', 'yellow'))
                 exit()
         if args.jpg:
             args.output = f"{base_name}.jpg" # The previous change was just for comparison. This reverts back, then it's changed again in the process_file function.
 
-        process_file(input_path, args.output, args.scale, args.compression, args.flip_tif)
+        process_file(input_path, args.output, args.scale, args.compression, args.flip_tif, args.jpg_quality)
